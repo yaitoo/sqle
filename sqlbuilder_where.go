8a7d078325508b65ca91bc0535fcd68d107dd838
Lz <imlangzi@qq.com>
2024-02-20 10:01:21 +0800

    chore(where): used shouldSkip instead skipTimes

--- sqlbuilder_where.go
+++ sqlbuilder_where.go
@@ -2,15 +2,12 @@ package sqle
 
 type WhereBuilder struct {
 	*Builder
-	written   bool
-	skipTimes int
+	written    bool
+	shouldSkip bool
 }
 
 func (wb *WhereBuilder) If(predicate bool) *WhereBuilder {
-	if !predicate {
-		wb.skipTimes++
-	}
-
+	wb.shouldSkip = !predicate
 	return wb
 }
 
@@ -28,8 +25,8 @@ func (wb *WhereBuilder) SQL(op string, cmd string) *WhereBuilder {
 		return wb
 	}
 
-	if wb.skipTimes > 0 {
-		wb.skipTimes--
+	if wb.shouldSkip {
+		wb.shouldSkip = false
 		return wb
 	}
 
