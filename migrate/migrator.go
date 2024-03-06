d8b991e80e3eb401c0f4a75f94718c0010294bd5
Lz <imlangzi@qq.com>
2024-03-06 16:21:00 +0800

    feat(rotate): added Rotate

--- migrate/migrator.go
+++ migrate/migrator.go
@@ -35,11 +35,24 @@ const TABLE_MIGRATIONS = "CREATE TABLE IF NOT EXISTS sqle_migrations(" +
 	"`scripts` text NOT NULL," +
 	"PRIMARY KEY (checksum));"
 
+const TABLE_ROTATIONS = "CREATE TABLE IF NOT EXISTS sqle_rotations(" +
+	"`checksum` varchar(32) NOT NULL," +
+	"`name` varchar(45) NOT NULL," +
+	"`rotated_name` varchar(8) NOT NULL," +
+	"`rotated_on` datetime NOT NULL," +
+	"`execution_time` varchar(25) NOT NULL," +
+	"PRIMARY KEY (checksum, rotated_name));"
+
 type Migrator struct {
-	db     *sqle.DB
+	dbs    []*sqle.DB
 	suffix string
 
-	Versions []Semver
+	Versions         []Semver
+	MonthlyRotations []Rotation
+	WeeklyRotations  []Rotation
+	DailyRotations   []Rotation
+
+	now func() time.Time
 }
 
 func (s *Migrator) Len() int      { return len(s.Versions) }
@@ -81,11 +94,12 @@ func (s *Migrator) Less(i, j int) bool {
 
 }
 
-func New(db *sqle.DB) *Migrator {
+func New(dbs ...*sqle.DB) *Migrator {
 	return &Migrator{
-		db:       db,
+		dbs:      dbs,
 		suffix:   ".sql",
 		Versions: make([]Semver, 0, 25),
+		now:      time.Now,
 	}
 }
 
@@ -98,7 +112,8 @@ func (m *Migrator) Discover(fsys fs.FS, options ...Option) error {
 			return err
 		}
 		if d.IsDir() {
-			matches := regexpSemver.FindStringSubmatch(d.Name())
+			dn := d.Name()
+			matches := regexpSemver.FindStringSubmatch(dn)
 
 			//full/Major/Minor/Patch/Prerelease/Build
 			if len(matches) == 6 {
@@ -130,68 +145,32 @@ func (m *Migrator) Discover(fsys fs.FS, options ...Option) error {
 						continue
 					}
 
-					mi := Migration{}
-
-					matches := regexpChange.FindStringSubmatch(name)
-					if matches == nil {
-						return ErrInvalidScriptName
-					}
-
-					o, err := strconv.Atoi(matches[1])
-					if err != nil {
-						return ErrInvalidScriptName
-					}
-
-					mi.Rank = o
-					mi.Name = matches[2]
-
-					buf, err := fs.ReadFile(fsys, filepath.Join(path, name))
+					mi, err := m.loadMigration(name, fsys, path)
 					if err != nil {
 						return err
-					}
-
-					h := md5.New()
-					h.Write(buf)
-
-					mi.Checksum = fmt.Sprintf("%x", h.Sum(nil))
-					mi.Scripts = string(buf)
-
-					s := bufio.NewScanner(strings.NewReader(mi.Scripts))
-					s.Split(bufio.ScanLines)
-
-					if s.Scan() {
-						l := strings.ReplaceAll(s.Text(), " ", "")
-						/*rotate:monthly=yyyyMMDD-yyyyMMDD*/
-						if strings.HasPrefix(l, "/*") && strings.HasSuffix(l, "*/") {
-							items := strings.Split(l, ":")
-							if len(items) == 2 && items[0] == "/*rotate" {
-								options := strings.Split(items[1][0:len(items[1])-2], "=")
-								if len(options) == 2 && len(options[1]) == 17 {
-									r := m.getRotate(options[0])
-									if r != shardid.NoRotate {
-										begin, end, err := m.getRotateRange(options[1])
-										if err == nil {
-											mi.Rotate = r
-											mi.RotateBegin = begin
-											mi.RotateEnd = end
-										}
-									}
-								}
-							}
-						}
-
 					}
 
 					v.Migrations = append(v.Migrations, mi)
 				}
 
-				if err != nil {
-					return err
-				}
-
 				sort.Sort(&v)
 				m.Versions = append(m.Versions, v)
 				return nil
+			} else if dn == "monthly" {
+				m.MonthlyRotations, err = m.loadRotations(fsys, path)
+				if err != nil {
+					return err
+				}
+			} else if dn == "weekly" {
+				m.WeeklyRotations, err = m.loadRotations(fsys, path)
+				if err != nil {
+					return err
+				}
+			} else if dn == "daily" {
+				m.DailyRotations, err = m.loadRotations(fsys, path)
+				if err != nil {
+					return err
+				}
 			}
 
 		}
@@ -206,6 +185,97 @@ func (m *Migrator) Discover(fsys fs.FS, options ...Option) error {
 	sort.Sort(m)
 
 	return nil
+}
+
+func (m *Migrator) loadMigration(name string, fsys fs.FS, path string) (Migration, error) {
+	mi := Migration{}
+
+	matches := regexpChange.FindStringSubmatch(name)
+	if matches == nil {
+		return Migration{}, ErrInvalidScriptName
+	}
+
+	o, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return Migration{}, ErrInvalidScriptName
+	}
+
+	mi.Rank = o
+	mi.Name = matches[2]
+
+	buf, err := fs.ReadFile(fsys, filepath.Join(path, name))
+	if err != nil {
+		return Migration{}, err
+	}
+
+	h := md5.New()
+	h.Write(buf)
+
+	mi.Checksum = fmt.Sprintf("%x", h.Sum(nil))
+	mi.Scripts = string(buf)
+
+	s := bufio.NewScanner(strings.NewReader(mi.Scripts))
+	s.Split(bufio.ScanLines)
+
+	if s.Scan() {
+		l := strings.ReplaceAll(s.Text(), " ", "")
+
+		/*rotate:monthly=yyyyMMDD-yyyyMMDD*/
+		if strings.HasPrefix(l, "/*") && strings.HasSuffix(l, "*/") {
+			items := strings.Split(l, ":")
+			if len(items) == 2 && items[0] == "/*rotate" {
+				options := strings.Split(items[1][0:len(items[1])-2], "=")
+				if len(options) == 2 && len(options[1]) == 17 {
+					r := m.getRotate(options[0])
+					if r != shardid.NoRotate {
+						begin, end, err := m.getRotateRange(options[1])
+						if err == nil {
+							mi.Rotate = r
+							mi.RotateBegin = begin
+							mi.RotateEnd = end
+						}
+					}
+				}
+			}
+		}
+
+	}
+	return mi, nil
+}
+
+func (m *Migrator) loadRotations(fsys fs.FS, d string) ([]Rotation, error) {
+	files, err := fs.ReadDir(fsys, d)
+	if err != nil {
+		return nil, err
+	}
+
+	var items []Rotation
+	var s string
+
+	for _, di := range files {
+		dn := di.Name()
+		buf, err := fs.ReadFile(fsys, filepath.Join(d, dn))
+		if err != nil {
+			return nil, err
+		}
+
+		s = string(buf)
+		if strings.Contains(s, "<rotate>") {
+			var it Rotation
+
+			it.Name = dn[0 : len(dn)-len(filepath.Ext(dn))]
+			it.Script = s
+
+			h := md5.New()
+			h.Write(buf)
+
+			it.Checksum = fmt.Sprintf("%x", h.Sum(nil))
+
+			items = append(items, it)
+		}
+	}
+
+	return items, nil
 }
 
 func (m *Migrator) getRotate(option string) shardid.TableRotate {
@@ -260,9 +330,19 @@ func (m *Migrator) getRotateRange(option string) (time.Time, time.Time, error) {
 }
 
 func (m *Migrator) Init(ctx context.Context) error {
-	_, err := m.db.ExecContext(ctx, TABLE_MIGRATIONS)
+	for _, db := range m.dbs {
+		_, err := db.ExecContext(ctx, TABLE_MIGRATIONS)
+		if err != nil {
+			return err
+		}
 
-	return err
+		_, err = db.ExecContext(ctx, TABLE_ROTATIONS)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *Migrator) round(d time.Duration) time.Duration {
@@ -280,13 +360,31 @@ func (m *Migrator) round(d time.Duration) time.Duration {
 
 func (m *Migrator) Migrate(ctx context.Context) error {
 	var err error
-	log.Println("migrate:")
+	n := len(m.dbs)
+	for i, db := range m.dbs {
+		if n == 1 {
+			log.Println("migrate:")
+		} else {
+			log.Printf("migrate db%v:\n", i)
+		}
+
+		err = m.migrate(ctx, db)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Migrator) migrate(ctx context.Context, db *sqle.DB) error {
+	var err error
 
 	for _, v := range m.Versions {
 		n := len(v.Migrations)
 		w := len(strconv.Itoa(n))
 		log.Printf("┌─[ v%s ]\n", v.Name)
-		err = m.db.Transaction(ctx, nil, func(ctx context.Context, tx *sqle.Tx) error {
+		err = db.Transaction(ctx, nil, func(ctx context.Context, tx *sqle.Tx) error {
 
 			var checksum string
 
@@ -365,6 +463,124 @@ func (m *Migrator) Migrate(ctx context.Context) error {
 					}
 				}
 
+			}
+
+			log.Println("└────────────────────────────────────────────────────────────────")
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func (m *Migrator) Rotate(ctx context.Context) error {
+	var err error
+	n := len(m.dbs)
+	for i, db := range m.dbs {
+		if n == 1 {
+			log.Println("rotate:")
+		} else {
+			log.Printf("rotate db%v:\n", i)
+		}
+
+		now := m.now().UTC()
+		months := []string{
+			"_" + now.Format("200601"),
+			"_" + now.AddDate(0, 1, 0).Format("200601"),
+		}
+
+		err = m.rotate(ctx, db, "monthly", months, m.MonthlyRotations)
+		if err != nil {
+			return err
+		}
+
+		var week int
+		_, week = now.ISOWeek() //1-53 week
+
+		next := now.AddDate(0, 0, 7)
+		_, nextWeek := next.ISOWeek()
+
+		weeks := []string{
+			"_" + now.Format("2006") + fmt.Sprintf("%03d", week),
+			"_" + next.Format("2006") + fmt.Sprintf("%03d", nextWeek),
+		}
+
+		err = m.rotate(ctx, db, "weekly", weeks, m.WeeklyRotations)
+		if err != nil {
+			return err
+		}
+
+		days := []string{
+			"_" + now.Format("20060102"),
+			"_" + now.AddDate(0, 0, 1).Format("20060102"),
+		}
+
+		err = m.rotate(ctx, db, "daily", days, m.DailyRotations)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func (m *Migrator) rotate(ctx context.Context, db *sqle.DB, rotatedPeriod string, rotatedNames []string, rotations []Rotation) error {
+	var err error
+	var n int
+	var w int
+	var checksum string
+	for _, r := range rotations {
+		err = db.Transaction(ctx, nil, func(ctx context.Context, tx *sqle.Tx) error {
+			n = len(rotatedNames)
+			w = len(strconv.Itoa(n))
+			log.Printf("┌─[ %s: %s ]\n", rotatedPeriod, r.Name)
+
+			for i, rn := range rotatedNames {
+				err = tx.QueryRow("SELECT `checksum` FROM `sqle_rotations` WHERE `checksum` = ? and `rotated_name` = ?", r.Checksum, rn).Scan(&checksum)
+				if err != nil {
+					if !errors.Is(err, sql.ErrNoRows) {
+						return err
+					}
+				}
+
+				if checksum != "" {
+					log.Printf("│ »[%*d/%d] %-35s %-10s [✔]", w, i+1, n, rn, "")
+					continue
+				}
+
+				now := time.Now()
+
+				_, err = tx.Exec(strings.ReplaceAll(r.Script, "<rotate>", rn) + ";")
+				if err != nil {
+					return err
+				}
+
+				cmd := sqle.New()
+				et := m.round(time.Since(now)).String()
+				cmd.Insert("sqle_rotations").
+					Set("checksum", r.Checksum).
+					Set("name", r.Name).
+					Set("rotated_name", rn).
+					Set("rotated_on", now).
+					Set("execution_time", et).
+					End()
+
+				query, args, err := cmd.Build()
+				if err != nil {
+					return err
+				}
+				_, err = tx.ExecContext(ctx, query, args...)
+				if err != nil {
+					return err
+				}
+
+				log.Printf("│ »[%*d/%d] %-35s %-10s [+]\n", w, i+1, n, rn, et)
 			}
 
 			log.Println("└────────────────────────────────────────────────────────────────")
--- migrate/migrator_test.go
+++ migrate/migrator_test.go
@@ -138,7 +138,7 @@ func TestDiscover(t *testing.T) {
 			},
 		},
 		{
-			name: "rotate_should_work",
+			name: "rotate_marks_should_be_parsed",
 			fsys: fstest.MapFS{
 				"1.1.2/1_create_table_members.sql": &fstest.MapFile{
 					Data: []byte(`/* rotate:monthly=20240201-20240401 */
@@ -207,6 +207,56 @@ CREATE TABLE members`, members.Scripts)
 				require.Equal(t, shardid.MonthlyRotate, members.Rotate)
 				require.Equal(t, begin, members.RotateBegin)
 				require.Equal(t, end, members.RotateEnd)
+
+			},
+		},
+		{
+			name: "rotation_should_work",
+			fsys: fstest.MapFS{
+				"monthly/members.sql": &fstest.MapFile{
+					Data: []byte(`CREATE TABLE members<rotate>`),
+				},
+				"monthly/users.sql": &fstest.MapFile{
+					Data: []byte(`CREATE TABLE users<rotate>`),
+				},
+				"monthly/invalid_users.sql": &fstest.MapFile{
+					Data: []byte(`CREATE TABLE users`),
+				},
+				"weekly/orders.sql": &fstest.MapFile{
+					Data: []byte(`CREATE TABLE orders<rotate>`),
+				},
+				"weekly/invalid_orders.sql": &fstest.MapFile{
+					Data: []byte(`CREATE TABLE orders`),
+				},
+				"daily/login.sql": &fstest.MapFile{
+					Data: []byte("CREATE TABLE login<rotate>"),
+				},
+				"daily/invalid_login.sql": &fstest.MapFile{
+					Data: []byte("CREATE TABLE login"),
+				},
+				"daily/logs.sql": &fstest.MapFile{
+					Data: []byte("CREATE TABLE logs<rotate>"),
+				},
+			},
+			setup: func(db *sql.DB) *Migrator {
+				return New(sqle.Open(db))
+			},
+			assert: func(m *Migrator, t *testing.T) {
+				require.Len(t, m.MonthlyRotations, 2)
+				require.Equal(t, "members", m.MonthlyRotations[0].Name)
+				require.Equal(t, "CREATE TABLE members<rotate>", m.MonthlyRotations[0].Script)
+				require.Equal(t, "users", m.MonthlyRotations[1].Name)
+				require.Equal(t, "CREATE TABLE users<rotate>", m.MonthlyRotations[1].Script)
+
+				require.Len(t, m.WeeklyRotations, 1)
+				require.Equal(t, "orders", m.WeeklyRotations[0].Name)
+				require.Equal(t, "CREATE TABLE orders<rotate>", m.WeeklyRotations[0].Script)
+
+				require.Len(t, m.DailyRotations, 2)
+				require.Equal(t, "login", m.DailyRotations[0].Name)
+				require.Equal(t, "CREATE TABLE login<rotate>", m.DailyRotations[0].Script)
+				require.Equal(t, "logs", m.DailyRotations[1].Name)
+				require.Equal(t, "CREATE TABLE logs<rotate>", m.DailyRotations[1].Script)
 
 			},
 		},
@@ -282,7 +332,7 @@ func TestMigrate(t *testing.T) {
 					Salt   string
 				}
 
-				err := m.db.QueryRow("SELECT id,status,email,passwd,salt FROM members WHERE id=?", 0).Bind(&Member{})
+				err := m.dbs[0].QueryRow("SELECT id,status,email,passwd,salt FROM members WHERE id=?", 0).Bind(&Member{})
 				require.ErrorIs(t, err, sql.ErrNoRows)
 
 				type Role struct {
@@ -290,7 +340,7 @@ func TestMigrate(t *testing.T) {
 					Name string
 				}
 
-				err = m.db.QueryRow("SELECT id,name FROM roles WHERE id=?", 0).Bind(&Role{})
+				err = m.dbs[0].QueryRow("SELECT id,name FROM roles WHERE id=?", 0).Bind(&Role{})
 				require.ErrorIs(t, err, sql.ErrNoRows)
 
 			},
@@ -336,7 +386,7 @@ func TestMigrate(t *testing.T) {
 					Salt   string
 				}
 
-				err := m.db.QueryRow("SELECT id,status,email,passwd,salt FROM members WHERE id=?", 0).Bind(&Member{})
+				err := m.dbs[0].QueryRow("SELECT id,status,email,passwd,salt FROM members WHERE id=?", 0).Bind(&Member{})
 				require.ErrorIs(t, err, sql.ErrNoRows)
 
 				type Role struct {
@@ -344,7 +394,7 @@ func TestMigrate(t *testing.T) {
 					Name string
 				}
 
-				err = m.db.QueryRow("SELECT id,name FROM roles WHERE id=?", 0).Bind(&Role{})
+				err = m.dbs[0].QueryRow("SELECT id,name FROM roles WHERE id=?", 0).Bind(&Role{})
 				require.ErrorIs(t, err, sql.ErrNoRows)
 
 			},
@@ -381,7 +431,7 @@ func TestMigrate(t *testing.T) {
 				}
 
 				for _, rt := range rotations {
-					err := m.db.QueryRow("SELECT id FROM monthly_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					err := m.dbs[0].QueryRow("SELECT id FROM monthly_logs"+rt+" WHERE id=?", 0).Scan(&id)
 					require.ErrorIs(t, err, sql.ErrNoRows)
 				}
 
@@ -419,7 +469,7 @@ func TestMigrate(t *testing.T) {
 				}
 
 				for _, rt := range rotations {
-					err := m.db.QueryRow("SELECT id FROM weekly_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					err := m.dbs[0].QueryRow("SELECT id FROM weekly_logs"+rt+" WHERE id=?", 0).Scan(&id)
 					require.ErrorIs(t, err, sql.ErrNoRows)
 				}
 
@@ -457,7 +507,7 @@ func TestMigrate(t *testing.T) {
 				}
 
 				for _, rt := range rotations {
-					err := m.db.QueryRow("SELECT id FROM daily_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					err := m.dbs[0].QueryRow("SELECT id FROM daily_logs"+rt+" WHERE id=?", 0).Scan(&id)
 					require.ErrorIs(t, err, sql.ErrNoRows)
 				}
 
@@ -494,11 +544,11 @@ func TestMigrate(t *testing.T) {
 					"_20240201", "_20240202", "_20240203", "_20240204", "_20240205", "_20240206",
 				}
 
-				err := m.db.QueryRow("SELECT id FROM no_logs WHERE id=?", 0).Scan(&id)
+				err := m.dbs[0].QueryRow("SELECT id FROM no_logs WHERE id=?", 0).Scan(&id)
 				require.ErrorIs(t, err, sql.ErrNoRows)
 
 				for _, rt := range rotations {
-					err := m.db.QueryRow("SELECT id FROM daily_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					err := m.dbs[0].QueryRow("SELECT id FROM daily_logs"+rt+" WHERE id=?", 0).Scan(&id)
 					require.ErrorContains(t, err, "no such table")
 				}
 
@@ -520,6 +570,157 @@ func TestMigrate(t *testing.T) {
 			require.NoError(t, err)
 			// Nothing should be changed on re-migration
 			err = m.Migrate(context.TODO())
+			require.NoError(t, err)
+
+			test.assert(t, m)
+		})
+	}
+
+}
+
+func TestRotate(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		setup  func(db *sql.DB) (*Migrator, error)
+		assert func(t *testing.T, m *Migrator)
+	}{
+		{
+			name: "monthly_rotate_should_work",
+			setup: func(db *sql.DB) (*Migrator, error) {
+
+				m := New(sqle.Open(db))
+				m.now = func() time.Time {
+					return time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+				}
+
+				err := m.Discover(fstest.MapFS{
+					"monthly/monthly_logs.sql": &fstest.MapFile{
+						Data: []byte(`CREATE TABLE IF NOT EXISTS monthly_logs<rotate> (
+							id int NOT NULL,
+							msg varchar(50) NOT NULL,
+							PRIMARY KEY (id)
+						);`),
+					},
+				})
+
+				if err != nil {
+					return nil, err
+				}
+
+				return m, nil
+
+			},
+			assert: func(t *testing.T, m *Migrator) {
+				var id int64
+
+				rotations := []string{
+					"_202402", "_202403",
+				}
+
+				for _, rt := range rotations {
+					err := m.dbs[0].QueryRow("SELECT id FROM monthly_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					require.ErrorIs(t, err, sql.ErrNoRows)
+				}
+
+			},
+		},
+		{
+			name: "weekly_rotate_should_work",
+			setup: func(db *sql.DB) (*Migrator, error) {
+
+				m := New(sqle.Open(db))
+				m.now = func() time.Time {
+					return time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+				}
+
+				err := m.Discover(fstest.MapFS{
+					"weekly/weekly_logs.sql": &fstest.MapFile{
+						Data: []byte(`CREATE TABLE IF NOT EXISTS weekly_logs<rotate> (
+							id int NOT NULL,
+							msg varchar(50) NOT NULL,
+							PRIMARY KEY (id)
+						);`),
+					},
+				})
+
+				if err != nil {
+					return nil, err
+				}
+
+				return m, nil
+
+			},
+			assert: func(t *testing.T, m *Migrator) {
+				var id int64
+
+				rotations := []string{
+					"_2024005", "_2024006",
+				}
+
+				for _, rt := range rotations {
+					err := m.dbs[0].QueryRow("SELECT id FROM weekly_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					require.ErrorIs(t, err, sql.ErrNoRows)
+				}
+
+			},
+		},
+		{
+			name: "daily_rotate_should_work",
+			setup: func(db *sql.DB) (*Migrator, error) {
+
+				m := New(sqle.Open(db))
+				m.now = func() time.Time {
+					return time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+				}
+
+				err := m.Discover(fstest.MapFS{
+					"daily/daily_logs.sql": &fstest.MapFile{
+						Data: []byte(`CREATE TABLE IF NOT EXISTS daily_logs<rotate> (
+							id int NOT NULL,
+							msg varchar(50) NOT NULL,
+							PRIMARY KEY (id)
+						);`),
+					},
+				})
+
+				if err != nil {
+					return nil, err
+				}
+
+				return m, nil
+
+			},
+			assert: func(t *testing.T, m *Migrator) {
+				var id int64
+
+				rotations := []string{
+					"_20240201", "_20240202",
+				}
+
+				for _, rt := range rotations {
+					err := m.dbs[0].QueryRow("SELECT id FROM daily_logs"+rt+" WHERE id=?", 0).Scan(&id)
+					require.ErrorIs(t, err, sql.ErrNoRows)
+				}
+
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			db, clean, err := createSqlite3()
+			defer clean()
+			require.NoError(t, err)
+
+			m, err := test.setup(db)
+			require.NoError(t, err)
+			err = m.Init(context.TODO())
+			require.NoError(t, err)
+			err = m.Rotate(context.TODO())
+			require.NoError(t, err)
+			// Nothing should be changed on re-rotated
+			err = m.Rotate(context.TODO())
 			require.NoError(t, err)
 
 			test.assert(t, m)
