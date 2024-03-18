6c089083d9f2d1399b906c273fb48807587ccb78
Lz <imlangzi@qq.com>
2024-03-18 22:15:33 +0800

    feat(db): added Duration support in Scanner/Valuer

---
+++ duration.go
@@ -0,0 +1,43 @@
+package sqle
+
+import (
+	"database/sql/driver"
+	"errors"
+	"time"
+)
+
+type Duration time.Duration
+
+// Value implements the driver.Valuer interface,
+// and turns the Duration into a VARCHAR field  for MySQL storage.
+func (d Duration) Value() (driver.Value, error) {
+	return time.Duration(d).String(), nil
+}
+
+// Scan implements the sql.Scanner interface,
+// and turns the VARCHAR field incoming from MySQL into a Duration
+func (d *Duration) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+
+	var val string
+
+	switch v := src.(type) {
+	case []byte:
+		val = string(v)
+	case string:
+		val = v
+	default:
+		return errors.New("bad duration type assertion")
+	}
+
+	td, err := time.ParseDuration(val)
+	if err != nil {
+		return err
+	}
+
+	*d = Duration(td)
+
+	return nil
+}
---
+++ duration_test.go
@@ -0,0 +1,75 @@
+package sqle
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDuration(t *testing.T) {
+	d, err := sql.Open("sqlite3", "file::memory:")
+	require.NoError(t, err)
+
+	_, err = d.Exec("CREATE TABLE `users` (`id` id NOT NULL,`ttl` VARCHAR(20), PRIMARY KEY (`id`))")
+	require.NoError(t, err)
+
+	d10 := Duration(10 * time.Second)
+	d11 := Duration(11 * time.Second)
+	d12 := Duration(12 * time.Second)
+	d13 := Duration(13 * time.Second)
+
+	result, err := d.Exec("INSERT INTO `users`(`id`, `ttl`) VALUES(?, ?)", 10, d10)
+	require.NoError(t, err)
+
+	rows, err := result.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), rows)
+
+	result, err = d.Exec("INSERT INTO `users`(`id`, `ttl`) VALUES(?, ?)", 11, d11)
+	require.NoError(t, err)
+
+	rows, err = result.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), rows)
+
+	result, err = d.Exec("INSERT INTO `users`(`id`, `ttl`) VALUES(?, ?)", 12, d12)
+	require.NoError(t, err)
+
+	rows, err = result.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), rows)
+
+	result, err = d.Exec("INSERT INTO `users`(`id`, `ttl`) VALUES(?, ?)", 13, d13)
+	require.NoError(t, err)
+
+	rows, err = result.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), rows)
+
+	var b10 Duration
+	err = d.QueryRow("SELECT `ttl` FROM `users` WHERE id=?", 10).Scan(&b10)
+	require.NoError(t, err)
+
+	require.EqualValues(t, d10, b10)
+
+	var b11 Duration
+	err = d.QueryRow("SELECT `ttl` FROM `users` WHERE id=?", 11).Scan(&b11)
+	require.NoError(t, err)
+
+	require.EqualValues(t, d11, b11)
+
+	var b12 Duration
+	err = d.QueryRow("SELECT `ttl` FROM `users` WHERE id=?", 12).Scan(&b12)
+	require.NoError(t, err)
+
+	require.EqualValues(t, d12, b12)
+
+	var b13 Duration
+	err = d.QueryRow("SELECT `ttl` FROM `users` WHERE id=?", 13).Scan(&b13)
+	require.NoError(t, err)
+
+	require.EqualValues(t, d13, b13)
+
+}
