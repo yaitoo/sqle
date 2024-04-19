bb41e816c9c9a65accf01d98c8b964f72201e29e
Lz <imlangzi@qq.com>
2024-04-19 10:35:30 +0800

    fix(query): renamed LimitOption/LimitResult

--- limit_option.go
+++ limit_option.go
@@ -1,25 +1,25 @@
 package sqle
 
-// LimitedQueryOption is a function type that modifies a LimitedQuery.
-type LimitedQueryOption func(q *LimitedQuery)
+// LimitOption is a function type that modifies a LimitedQuery.
+type LimitOption func(q *LimitOptions)
 
-// LimitedQuery represents a query with pagination and ordering options.
-type LimitedQuery struct {
+// LimitOptions represents a query with pagination and ordering options.
+type LimitOptions struct {
 	Offset  int64           // PageIndex represents the index of the page to retrieve.
 	Limit   int64           // PageSize represents the number of items per page.
 	OrderBy *OrderByBuilder // OrderBy represents the ordering of the query.
 }
 
 // WithPageSize is a LimitedQueryOption that sets the page size of the LimitedQuery.
-func WithPageSize(size int64) LimitedQueryOption {
-	return func(q *LimitedQuery) {
+func WithPageSize(size int64) LimitOption {
+	return func(q *LimitOptions) {
 		q.Limit = size
 	}
 }
 
 // WithOrderBy is a LimitedQueryOption that sets the ordering of the LimitedQuery.
-func WithOrderBy(ob *OrderByBuilder) LimitedQueryOption {
-	return func(q *LimitedQuery) {
+func WithOrderBy(ob *OrderByBuilder) LimitOption {
+	return func(q *LimitOptions) {
 		q.OrderBy = ob
 	}
 }
