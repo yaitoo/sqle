328fae80e16106aa75473dab4b4f91ec59dde92a
Lz <imlangzi@qq.com>
2024-04-26 11:11:37 +0800

    fix(bool): renamed BitBool with Bool

--- bool_test.go
+++ bool_test.go
@@ -7,21 +7,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBitBool(t *testing.T) {
+func TestBool(t *testing.T) {
 	d, err := sql.Open("sqlite3", "file::memory:")
 	require.NoError(t, err)
 
 	_, err = d.Exec("CREATE TABLE `users` (`id` id NOT NULL,`status` BIT(1), PRIMARY KEY (`id`))")
 	require.NoError(t, err)
 
-	result, err := d.Exec("INSERT INTO `users`(`id`, `status`) VALUES(?, ?)", 10, BitBool(true))
+	result, err := d.Exec("INSERT INTO `users`(`id`, `status`) VALUES(?, ?)", 10, Bool(true))
 	require.NoError(t, err)
 
 	rows, err := result.RowsAffected()
 	require.NoError(t, err)
 	require.Equal(t, int64(1), rows)
 
-	result, err = d.Exec("INSERT INTO `users`(`id`, `status`) VALUES(?, ?)", 11, BitBool(false))
+	result, err = d.Exec("INSERT INTO `users`(`id`, `status`) VALUES(?, ?)", 11, Bool(false))
 	require.NoError(t, err)
 
 	rows, err = result.RowsAffected()
@@ -42,25 +42,25 @@ func TestBitBool(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, int64(1), rows)
 
-	var b1 BitBool
+	var b1 Bool
 	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=?", 10).Scan(&b1)
 	require.NoError(t, err)
 
 	require.EqualValues(t, true, b1)
 
-	var b2 BitBool
+	var b2 Bool
 	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=?", 11).Scan(&b2)
 	require.NoError(t, err)
 
 	require.EqualValues(t, false, b2)
 
-	var b3 BitBool
+	var b3 Bool
 	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=?", 12).Scan(&b3)
 	require.NoError(t, err)
 
 	require.EqualValues(t, true, b3)
 
-	var b4 BitBool
+	var b4 Bool
 	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=?", 13).Scan(&b4)
 	require.NoError(t, err)
 
