9bad96eb7a8b424e573f6c9cff9f85b935316bf6
Lz <imlangzi@qq.com>
2025-09-05 09:39:56 +0800

    fix(bool): fixed driver.Value and sql.Scanner issues on bool (#52)

--- bool.go
+++ bool.go
@@ -2,7 +2,7 @@ package sqle
 
 import (
 	"database/sql/driver"
-	"errors"
+	"strings"
 )
 
 // Bool is an implementation of a bool for the MySQL type BIT(1).
@@ -11,11 +11,7 @@ type Bool bool
 // Value implements the driver.Valuer interface,
 // and turns the BitBool into a bit field (BIT(1)) for MySQL storage.
 func (b Bool) Value() (driver.Value, error) { // skipcq: GO-W1029
-	if b {
-		return []byte{1}, nil
-	} else {
-		return []byte{0}, nil
-	}
+	return bool(b), nil
 }
 
 // Scan implements the sql.Scanner interface,
@@ -30,8 +26,14 @@ func (b *Bool) Scan(src interface{}) error { // skipcq: GO-W1029
 		*b = v[0] == 1
 	case int64:
 		*b = v == 1
-	default:
-		return errors.New("bad []byte type assertion")
+	case bool:
+		*b = Bool(v)
+	case string:
+		if v == "1" || strings.EqualFold(v, "t") || strings.EqualFold(v, "true") {
+			*b = true
+		} else {
+			*b = false
+		}
 	}
 
 	return nil
--- bool_test.go
+++ bool_test.go
@@ -11,7 +11,7 @@ func TestBool(t *testing.T) {
 	d, err := sql.Open("sqlite3", "file::memory:")
 	require.NoError(t, err)
 
-	_, err = d.Exec("CREATE TABLE `users` (`id` int NOT NULL,`status` BIT(1), PRIMARY KEY (`id`))")
+	_, err = d.Exec("CREATE TABLE `users` (`id` int NOT NULL,`status` BIT(1),`status_string` VARCHAR(5),`status_int` INT, `status_boolean` boolean, PRIMARY KEY (`id`))")
 	require.NoError(t, err)
 
 	result, err := d.Exec("INSERT INTO `users`(`id`, `status`) VALUES(?, ?)", 10, Bool(true))
@@ -48,6 +48,11 @@ func TestBool(t *testing.T) {
 
 	require.EqualValues(t, true, b1)
 
+	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=? AND status=1", 10).Scan(&b1)
+	require.NoError(t, err)
+
+	require.EqualValues(t, true, b1)
+
 	var b2 Bool
 	err = d.QueryRow("SELECT `status` FROM `users` WHERE id=?", 11).Scan(&b2)
 	require.NoError(t, err)
@@ -65,5 +70,60 @@ func TestBool(t *testing.T) {
 	require.NoError(t, err)
 
 	require.EqualValues(t, false, b4)
+
+	// Insert with different types
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_string`) VALUES(?, ?)", 14, "true")
+	require.NoError(t, err)
+
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_string`) VALUES(?, ?)", 15, "false")
+	require.NoError(t, err)
+
+	var b5 Bool
+	err = d.QueryRow("SELECT `status_string` FROM `users` WHERE id=?", 14).Scan(&b5)
+	require.NoError(t, err)
+
+	require.EqualValues(t, true, b5)
+
+	var b6 Bool
+	err = d.QueryRow("SELECT `status_string` FROM `users` WHERE id=?", 15).Scan(&b6)
+	require.NoError(t, err)
+
+	require.EqualValues(t, false, b6)
+
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_int`) VALUES(?, ?)", 16, 1)
+	require.NoError(t, err)
+
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_int`) VALUES(?, ?)", 17, 0)
+	require.NoError(t, err)
+
+	var b7 Bool
+	err = d.QueryRow("SELECT `status_int` FROM `users` WHERE id=?", 16).Scan(&b7)
+	require.NoError(t, err)
+
+	require.EqualValues(t, true, b7)
+
+	var b8 Bool
+	err = d.QueryRow("SELECT `status_int` FROM `users` WHERE id=?", 17).Scan(&b8)
+	require.NoError(t, err)
+
+	require.EqualValues(t, false, b8)
+
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_boolean`) VALUES(?, ?)", 18, true)
+	require.NoError(t, err)
+
+	_, err = d.Exec("INSERT INTO `users`(`id`, `status_boolean`) VALUES(?, ?)", 19, false)
+	require.NoError(t, err)
+
+	var b9 Bool
+	err = d.QueryRow("SELECT `status_boolean` FROM `users` WHERE id=?", 18).Scan(&b9)
+	require.NoError(t, err)
+
+	require.EqualValues(t, true, b9)
+
+	var b10 Bool
+	err = d.QueryRow("SELECT `status_boolean` FROM `users` WHERE id=?", 19).Scan(&b10)
+	require.NoError(t, err)
+
+	require.EqualValues(t, false, b10)
 
 }
